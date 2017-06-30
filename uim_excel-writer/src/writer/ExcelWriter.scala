9eaf31a8a93fede1cb331eccd3925596a955787f
ordinarygithubuser <madseahorse@yandex.com>
2017-06-30 15:17:05 +0200

    Exceldatei wird nun aus einer Serie Testdaten generiert.

--- uim_excel-writer/src/writer/ExcelWriter.scala
+++ uim_excel-writer/src/writer/ExcelWriter.scala
@@ -5,39 +5,22 @@ import java.io.{File, FileInputStream, FileOutputStream}
 import org.apache.poi.xssf.usermodel.{XSSFSheet, XSSFWorkbook}
 
 class Test (val image1: File, val image2: File, val similarity: Float) {
-  def isSameClass(): Boolean = {
-    image1.getParentFile.equals(image2.getParentFile)
-  }
+  def isSameClass: Boolean = image1.getParentFile.equals(image2.getParentFile)
 }
 
 class ExcelWriter {
 
   val RESULT_PATH = "./gen/roc_tests.xlsx"
+  val TEMPLATE_PATH = "roc_template.xlsx"
 
-  def write (tests: List[Test]): Unit = {
-    val streamIn = new FileInputStream("roc_template.xlsx")
-    val streamOut = new FileOutputStream(RESULT_PATH)
-    val workbook = new XSSFWorkbook(streamIn)
-    val kValues = List(5, 20, 50, 90, 200, 500, 1000)
+  private val streamIn = new FileInputStream(TEMPLATE_PATH)
+  private val workbook = new XSSFWorkbook(streamIn)
 
-    kValues.foreach { k =>
-      printTestSheet(tests, workbook.getSheet("k = " + k), k)
-    }
+  streamIn.close()
 
-    workbook.write(streamOut)
-    streamIn.close()
-    streamOut.close()
-  }
-
-  /**
-    * Copies the test data of tests to the XSSFWorksheet of the workbook at
-    * the supplied index.
-    *
-    * @param tests The test data
-    * @param sheet The sheet to write to
-    */
-  private def printTestSheet (tests: List[Test], sheet: XSSFSheet, k: Int) = {
+  def write (k: Int, tests: List[Test]): Unit = {
     val count = tests.length + 1
+    val sheet = workbook.getSheet("k = " + k)
     val first = sheet.createRow(0)
 
     first.createCell(0, 1).setCellValue("IMG1")
@@ -58,7 +41,7 @@ class ExcelWriter {
       row.createCell(0, 1).setCellValue(test.image1.getAbsolutePath)
       row.createCell(1, 1).setCellValue(test.image2.getAbsolutePath)
       row.createCell(2, 1).setCellFormula("RAND()")
-      row.createCell(3, 1).setCellValue(if (test.isSameClass()) "+" else "-")
+      row.createCell(3, 1).setCellValue(if (test.isSameClass) "+" else "-")
       row.createCell(4, 1).setCellValue(test.similarity)
       row.createCell(5, 2).setCellFormula(makePrecisionFormula(rowIndex + 1))
       row.createCell(6, 2).setCellFormula(makeTruePositiveFormula(rowIndex + 1, count))
@@ -71,30 +54,11 @@ class ExcelWriter {
     }
   }
 
-  /*
-  private def printResultSheet(resultSheet: XSSFSheet, sheets: List[XSSFSheet], count: Int) = {
-    val drawing = resultSheet.createDrawingPatriarch
-    val anchor = drawing.createAnchor(0, 0, 0, 0, 0, 5, 10, 15)
-    val chart = drawing.createChart(anchor)
-    val legend = chart.getOrCreateLegend
-    val data = chart.getChartDataFactory.createScatterChartData
-
-    val bottomAxis = chart.getChartAxisFactory.createCategoryAxis(AxisPosition.BOTTOM)
-    val leftAxis = chart.getChartAxisFactory.createValueAxis(AxisPosition.LEFT)
-
-    legend.setPosition(LegendPosition.TOP_RIGHT)
-    leftAxis.setCrosses(AxisCrosses.AUTO_ZERO)
-
-    sheets.foreach { dataSheet =>
-      val xs = DataSources.fromNumericCellRange(dataSheet, new CellRangeAddress(2, count, 7, 7))
-      val ys = DataSources.fromNumericCellRange(dataSheet, new CellRangeAddress(2, count, 6, 6))
-      val series = data.addSerie(xs, ys)
-      series.setTitle(dataSheet.getSheetName)
-    }
-
-    chart.plot(data, bottomAxis, leftAxis)
+  def save (): Unit = {
+    val streamOut = new FileOutputStream(RESULT_PATH)
+    workbook.write(streamOut)
+    streamOut.close()
   }
-  */
 
   private def makePrecisionFormula (rowIndex: Int): String = {
     "COUNTIF($D$2:D" + rowIndex + ",\"+\")/COUNTA($D$2:$D" + rowIndex + ")"
--- uim_excel-writer/src/writer/Main.scala
+++ uim_excel-writer/src/writer/Main.scala
@@ -1,24 +1,55 @@
 package writer
 
-import java.io.File
+import java.io.{File, FileFilter}
 
 import scala.io.Source
 
 object Main {
 
-  def main(args: Array[String]): Unit = {
-    val path = args(0)
-    val writer = new ExcelWriter
+  val DEBUG = true
 
-    val tests = Source.fromFile(path).getLines().toList.map { line =>
-      val tokens = line.split(" ")
-      val image1 = new File(tokens(0))
-      val image2 = new File(tokens(1))
-      val similarity = if (tokens.length > 3) tokens(3).toFloat else 0.0001f
-      new Test(image1, image2, similarity)
+  def main(args: Array[String]): Unit = {
+    val file = new File(args(0))
+
+    if (!file.exists || !file.isDirectory) {
+      throw new Exception("Invalid source for image sets: " + file.getAbsoluteFile)
     }
 
-    writer.write(tests)
+    val writer = new ExcelWriter
+    val sets = readDir(file)
+
+    sets.foreach { set =>
+      val tests = Source.fromFile(set).getLines().toList.map { line =>
+        val tokens = line.split(" ")
+        val image1 = new File(tokens(0))
+        val image2 = new File(tokens(1))
+        val similarity = if (tokens.length > 3) tokens(3).toFloat else 0.0001f
+        new Test(image1, image2, similarity)
+      }.sortWith { (test1, test2) =>
+        test1.similarity < test2.similarity
+      }
+
+      writer.write(getK(set), tests)
+    }
+
+    writer.save()
   }
 
+  def readDir (path: File): List[File] = {
+    path.listFiles(new FileFilter {
+      override def accept(file: File): Boolean = {
+        file.isFile && file.getName.endsWith(".txt")
+      }
+    }).toList
+  }
+
+  def getK (file: File): Int = {
+    try {
+      Integer.parseInt(file.getName.replaceAll("[\\D]", ""))
+    } catch {
+      case _: Throwable => {
+        throw new Exception("Invalid name (no k specified):" + file.getName)
+      }
+    }
+  }
 }
