1ac0eb3b6c8e09d2aa8a5bce85c89cb81902982f
ordinarygithubuser <madseahorse@yandex.com>
2017-11-11 16:00:43 +0100

    Testgenerierung erfolgt nun für AE und BoVW separat. Tests aktualisiert.

--- uim_test-select/src/select/Printer.scala
+++ uim_test-select/src/select/Printer.scala
@@ -2,14 +2,17 @@ package select
 
 import java.io.{File, PrintWriter}
 
+import scala.collection.mutable.ListBuffer
+
 object Printer {
 
   def print(path: String, dataSet: DataSet): Unit = {
-    writeTests(path + "/test.txt", dataSet.test)
+    writeTestsBoVW(path + "/test_bovw.txt", dataSet.test)
+    writeTestsAE(path + "/test_ae.txt", dataSet.test)
     writeTrain(path + "/train.txt", dataSet.train)
   }
 
-  private def writeTests (path: String, tests: List[Test]) = {
+  private def writeTestsBoVW (path: String, tests: List[Test]) = {
     val writer = getWriter(path)
 
     tests.foreach { test =>
@@ -18,6 +21,23 @@ object Printer {
       val same = if (test.isSameClass) "+" else "-"
 
       writer.println(name1 + " " + name2 + " " + same)
+    }
+    writer.close()
+  }
+
+  private def writeTestsAE (path: String, tests: List[Test]) = {
+    val writer = getWriter(path)
+    val unique = ListBuffer[String]()
+
+    tests.foreach { test =>
+      val path1 = test.image1.getAbsolutePath
+      val path2 = test.image2.getAbsolutePath
+      if (!unique.contains(path1)) unique.+=(path1)
+      if (!unique.contains(path2)) unique.+=(path2)
+    }
+
+    unique.foreach { path =>
+      writer.println(path)
     }
     writer.close()
   }
