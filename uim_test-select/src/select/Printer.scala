7ed2165f02f55c490a6eedf07ee02d0c82984c28
ordinarygithubuser <madseahorse@yandex.com>
2017-11-14 19:57:57 +0100

    Nonsense-Generierung entfernt..

--- uim_test-select/src/select/Printer.scala
+++ uim_test-select/src/select/Printer.scala
@@ -7,12 +7,11 @@ import scala.collection.mutable.ListBuffer
 object Printer {
 
   def print(path: String, dataSet: DataSet): Unit = {
-    writeTestsBoVW(path + "/test_bovw.txt", dataSet.test)
-    writeTestsAE(path + "/test_ae.txt", dataSet.test)
+    writeTests(path + "/test.txt", dataSet.test)
     writeTrain(path + "/train.txt", dataSet.train)
   }
 
-  private def writeTestsBoVW (path: String, tests: List[Test]) = {
+  private def writeTests (path: String, tests: List[Test]) = {
     val writer = getWriter(path)
 
     tests.foreach { test =>
@@ -21,23 +20,6 @@ object Printer {
       val same = if (test.isSameClass) "+" else "-"
 
       writer.println(name1 + " " + name2 + " " + same)
-    }
-    writer.close()
-  }
-
-  private def writeTestsAE (path: String, tests: List[Test]) = {
-    val writer = getWriter(path)
-    val unique = ListBuffer[String]()
-
-    tests.foreach { test =>
-      val path1 = test.image1.getAbsolutePath
-      val path2 = test.image2.getAbsolutePath
-      if (!unique.contains(path1)) unique.+=(path1)
-      if (!unique.contains(path2)) unique.+=(path2)
-    }
-
-    unique.foreach { path =>
-      writer.println(path)
     }
     writer.close()
   }
